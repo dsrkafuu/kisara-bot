8933e86f744151273cb23decd0f7ced23f1b9990
dsrkafuu <21136293+dsrkafuu@users.noreply.github.com>
2025-03-07 21:46:12 +0800

    fix: organize code

--- app/messages/prompt.ts
+++ app/messages/prompt.ts
@@ -8,139 +8,171 @@ import { OB11Message } from '@napcat/onebot';
 
 /** LLM 问答中间件 */
 const middleware: OnionMiddleware<OB11Message> = async (data, ctx, next) => {
-  const { user_id } = data;
-  const fullSimpleText = getSimpleText(data);
-  const qaSourceTextSplits = fullSimpleText.split('那我问你');
-  const rpSourceTextSplits = fullSimpleText.split('锐评一下');
-  const trSourceTextSplits = fullSimpleText.split('帮我翻译');
+  const { user_id, message_type } = data;
 
-  if (qaSourceTextSplits.length > 1) {
-    // 单人 QQ 号限流，群组请求者 QQ 限流
-    let limitKey = `prompt_qa_private_${user_id}`;
-    if (data.message_type === 'group') {
-      limitKey = `prompt_qa_group_${user_id}`;
-    }
-    const rateLimiter = getRateLimiter(limitKey, 10);
-
-    const inputText = qaSourceTextSplits
-      .map((text) => text.trim())
-      .join(' ')
-      .trim();
-    if (inputText) {
-      try {
-        if (rateLimiter.check()) {
-          const systemPrompt = `${promptConfig.qa.join('；')}。`.trim();
-          const userPrompt = `${inputText}？`.trim();
-          logger.info(
-            'prompt',
-            `request system prompt:\n${systemPrompt}\nrequest user prompt:\n${userPrompt}`
-          );
-          const { content, think } = await requestLLM(systemPrompt, userPrompt);
-          logger.info(
-            'prompt',
-            `respond think:\n${think?.trim() || ''}\nrespond content:\n${content?.trim() || ''}`
-          );
-          await ctx.send(
-            [
-              getText(
-                clearifyText(content) || '这个问题太难了，换个问题问我吧'
-              ),
-            ],
-            { quoteSender: true }
-          );
-        }
-      } catch (e: any) {
-        logger.error('prompt', 'qa error', e);
-        await ctx.send([getText('那我问你出现内部错误，请联系木更一号')]);
-      }
-
-      // 不需要其他插件了
-      ctx.swap.prompt_qa = true;
-      return;
-    }
+  // 是否 at 其他人
+  const botQQId = data.self_id;
+  let isAtOthers = false;
+  if (typeof data.message !== 'string' && botQQId) {
+    isAtOthers =
+      message_type === 'group' &&
+      data.message.some((item) => {
+        return item.type === 'at' && `${item.data.qq}` !== `${botQQId}`;
+      });
   }
 
-  if (rpSourceTextSplits.length > 1) {
-    // 单人 QQ 号限流，群组请求者 QQ 限流
-    let limitKey = `prompt_rp_private_${user_id}`;
-    if (data.message_type === 'group') {
-      limitKey = `prompt_rp_group_${user_id}`;
-    }
-    const rateLimiter = getRateLimiter(limitKey, 10);
+  // 如果不是 at 其他人
+  if (!isAtOthers) {
+    const fullSimpleText = getSimpleText(data);
+    const qaSourceTextSplits = fullSimpleText.split('那我问你');
+    const rpSourceTextSplits = fullSimpleText.split('锐评一下');
+    const trSourceTextSplits = fullSimpleText.split('帮我翻译');
 
-    const inputText = rpSourceTextSplits
-      .map((text) => text.trim())
-      .join(' ')
-      .trim();
-    if (inputText) {
-      try {
-        if (rateLimiter.check()) {
-          const systemPrompt = `${promptConfig.rp.join('；')}。`.trim();
-          const userPrompt = `锐评一下${inputText}。`;
-          logger.info(
-            'prompt',
-            `request system prompt:\n${systemPrompt}\nrequest user prompt:\n${userPrompt}`
-          );
-          const { content, think } = await requestLLM(systemPrompt, userPrompt);
-          logger.info(
-            'prompt',
-            `respond think:\n${think?.trim() || ''}\nrespond content:\n${content?.trim() || ''}`
-          );
-          await ctx.send(
-            [getText(clearifyText(content) || '这个锐评不了，换个主题问我吧')],
-            { quoteSender: true }
-          );
-        }
-      } catch (e: any) {
-        logger.error('prompt', 'rp error', e);
-        await ctx.send([getText('锐评一下出现内部错误，请联系木更一号')]);
+    if (qaSourceTextSplits.length > 1) {
+      // 单人 QQ 号限流，群组请求者 QQ 限流
+      let limitKey = `prompt_qa_private_${user_id}`;
+      if (data.message_type === 'group') {
+        limitKey = `prompt_qa_group_${user_id}`;
       }
+      const rateLimiter = getRateLimiter(limitKey, 10);
 
-      // 不需要其他插件了
-      ctx.swap.prompt_rp = true;
-      return;
-    }
-  }
-
-  if (trSourceTextSplits.length > 1) {
-    // 单人 QQ 号限流，群组请求者 QQ 限流
-    let limitKey = `prompt_tr_private_${user_id}`;
-    if (data.message_type === 'group') {
-      limitKey = `prompt_tr_group_${user_id}`;
-    }
-    const rateLimiter = getRateLimiter(limitKey, 10);
-
-    const inputText = trSourceTextSplits
-      .map((text) => text.trim())
-      .join(' ')
-      .trim();
-    if (inputText) {
-      try {
-        if (rateLimiter.check()) {
-          const systemPrompt = `${promptConfig.tr.join('；')}。`.trim();
-          const userPrompt = `翻译下面的内容：\n${inputText}`.trim();
-          logger.info(
-            'prompt',
-            `request system prompt:\n${systemPrompt}\nrequest user prompt:\n${userPrompt}`
-          );
-          const { content, think } = await requestLLM(systemPrompt, userPrompt);
-          logger.info(
-            'prompt',
-            `respond think:\n${think?.trim() || ''}\nrespond content:\n${content?.trim() || ''}`
-          );
-          await ctx.send(
-            [getText(clearifyText(content) || '这个翻译不了，换个内容问我吧')],
-            { quoteSender: true }
-          );
+      const inputText = qaSourceTextSplits
+        .map((text) => text.trim())
+        .join(' ')
+        .trim();
+      if (inputText) {
+        try {
+          if (rateLimiter.check()) {
+            const systemPrompt = `${promptConfig.qa.join('；')}。`.trim();
+            const userPrompt = `${inputText}？`.trim();
+            logger.info(
+              'prompt',
+              `request system prompt:\n${systemPrompt}\nrequest user prompt:\n${userPrompt}`
+            );
+            const { content, think } = await requestLLM(
+              systemPrompt,
+              userPrompt
+            );
+            logger.info(
+              'prompt',
+              `respond think:\n${think?.trim() || ''}\nrespond content:\n${content?.trim() || ''}`
+            );
+            await ctx.send(
+              [
+                getText(
+                  clearifyText(content) || '这个问题太难了，换个问题问我吧'
+                ),
+              ],
+              { quoteSender: true }
+            );
+          }
+        } catch (e: any) {
+          logger.error('prompt', 'qa error', e);
+          await ctx.send([getText('那我问你出现内部错误，请联系木更一号')]);
         }
-      } catch (e: any) {
-        logger.error('prompt', 'tr error', e);
-        await ctx.send([getText('翻译出现内部错误，请联系木更一号')]);
-      }
 
-      // 不需要其他插件了
-      ctx.swap.prompt_tr = true;
-      return;
+        // 不需要其他插件了
+        ctx.swap.prompt_qa = true;
+        return;
+      }
+    }
+
+    if (rpSourceTextSplits.length > 1) {
+      // 单人 QQ 号限流，群组请求者 QQ 限流
+      let limitKey = `prompt_rp_private_${user_id}`;
+      if (data.message_type === 'group') {
+        limitKey = `prompt_rp_group_${user_id}`;
+      }
+      const rateLimiter = getRateLimiter(limitKey, 10);
+
+      const inputText = rpSourceTextSplits
+        .map((text) => text.trim())
+        .join(' ')
+        .trim();
+      if (inputText) {
+        try {
+          if (rateLimiter.check()) {
+            const systemPrompt = `${promptConfig.rp.join('；')}。`.trim();
+            const userPrompt = `锐评一下${inputText}。`;
+            logger.info(
+              'prompt',
+              `request system prompt:\n${systemPrompt}\nrequest user prompt:\n${userPrompt}`
+            );
+            const { content, think } = await requestLLM(
+              systemPrompt,
+              userPrompt
+            );
+            logger.info(
+              'prompt',
+              `respond think:\n${think?.trim() || ''}\nrespond content:\n${content?.trim() || ''}`
+            );
+            await ctx.send(
+              [
+                getText(
+                  clearifyText(content) || '这个锐评不了，换个主题问我吧'
+                ),
+              ],
+              { quoteSender: true }
+            );
+          }
+        } catch (e: any) {
+          logger.error('prompt', 'rp error', e);
+          await ctx.send([getText('锐评一下出现内部错误，请联系木更一号')]);
+        }
+
+        // 不需要其他插件了
+        ctx.swap.prompt_rp = true;
+        return;
+      }
+    }
+
+    if (trSourceTextSplits.length > 1) {
+      // 单人 QQ 号限流，群组请求者 QQ 限流
+      let limitKey = `prompt_tr_private_${user_id}`;
+      if (data.message_type === 'group') {
+        limitKey = `prompt_tr_group_${user_id}`;
+      }
+      const rateLimiter = getRateLimiter(limitKey, 10);
+
+      const inputText = trSourceTextSplits
+        .map((text) => text.trim())
+        .join(' ')
+        .trim();
+      if (inputText) {
+        try {
+          if (rateLimiter.check()) {
+            const systemPrompt = `${promptConfig.tr.join('；')}。`.trim();
+            const userPrompt = `翻译下面的内容：\n${inputText}`.trim();
+            logger.info(
+              'prompt',
+              `request system prompt:\n${systemPrompt}\nrequest user prompt:\n${userPrompt}`
+            );
+            const { content, think } = await requestLLM(
+              systemPrompt,
+              userPrompt
+            );
+            logger.info(
+              'prompt',
+              `respond think:\n${think?.trim() || ''}\nrespond content:\n${content?.trim() || ''}`
+            );
+            await ctx.send(
+              [
+                getText(
+                  clearifyText(content) || '这个翻译不了，换个内容问我吧'
+                ),
+              ],
+              { quoteSender: true }
+            );
+          }
+        } catch (e: any) {
+          logger.error('prompt', 'tr error', e);
+          await ctx.send([getText('翻译出现内部错误，请联系木更一号')]);
+        }
+
+        // 不需要其他插件了
+        ctx.swap.prompt_tr = true;
+        return;
+      }
     }
   }
 
